ba102fb7f6846ae6d09b9d5a5b0a294ace04cd55
RC0033888 <RC0033888@CRO-24-20>
2019-02-25 15:51:49 +0000

    Add project files.

---
+++ constants/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace constants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double pi = 3.141592653589793;
+            double c = 299792458;
+            double d1 = 5e6;
+            richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString("N") + "\n");
+            richTextBox1.AppendText(c.ToString("N3") + "\n");
+            richTextBox1.AppendText(c.ToString("NO") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
