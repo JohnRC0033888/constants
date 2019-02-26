fd655a0d0ff01711ed2f54f471b8d126ff982d04
RC0033888 <RC0033888@CRO-24-20>
2019-02-26 09:48:29 +0000

    ttdyfrturyufrt6u

--- constants/Form1.Designer.cs
+++ constants/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(164, 166);
+            this.richTextBox1.Size = new System.Drawing.Size(394, 334);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
--- constants/Form1.cs
+++ constants/Form1.cs
@@ -16,16 +16,28 @@ namespace constants
         {
             InitializeComponent();
             double pi = 3.141592653589793;
-            double c = 299792458;
+            double c  = 299792458;
             double d1 = 5e6;
             richTextBox1.AppendText(pi.ToString() + "\n");
             richTextBox1.AppendText(pi.ToString("N") + "\n");
             richTextBox1.AppendText(c.ToString("N3") + "\n");
-            richTextBox1.AppendText(c.ToString("NO") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
             richTextBox1.AppendText(d1.ToString("0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText(c.ToString("0") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
+
 
         }
 
